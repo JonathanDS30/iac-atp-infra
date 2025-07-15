49ca0993068744e62f806e3f8db69c43b9a30f65
JonathanDS30 <jonathan.dellasantina433@gmail.com>
2025-07-15 15:43:42 +0200

    feat: Update vm_debian module
    - add disk_file_format by default "qcow2"
    - add cpu type by default"host"
    - changing file name provider.tf ->providers.tf

---
+++ terraform/main.tf
@@ -0,0 +1,14 @@
+module "vm_debian1" {
+  source = "./modules/vm_debian"
+  node_name = "SRV-PMX"
+  template_id = 9101
+  vm_name = "TestVM"
+  disk_size = 50
+  datastore_id = "local-lvm"
+  ip_address = "192.168.1.50"
+  netmask = 24
+  gateway = "192.168.1.254"
+
+  username = "user-jd"
+  password = "test"
+}
